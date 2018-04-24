42f13f211c0db24f28e48c3cbacd494ff39da71f
Sam Martinez <sam.martinez.dev@gmail.com>
2018-04-23 19:54:55 -0500

    Fixes merge conflicts

--- isloggedin.php
+++ isloggedin.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     // log in user by checking whether the session variable username is set
 
@@ -21,28 +20,4 @@
     $response['username'] = $username;
     header('Content-Type: application/json');
     echo(json_encode($response));    
-=======
-<?php
-    // log in user by checking whether the session variable username is set
-
-    
-    session_start();
-    if (isset($_SESSION['username'])) {
-        // if the session variable username is set, then we are logged in
-        $isloggedin = true;
-        $username = $_SESSION['username'];
-    } else {
-        // if we are not logged in
-        $isloggedin = false;
-        $username = "not logged in";        
-    }
-
-    // send response back
-    $response = array();
-    $response['status'] = 'success';
-    $response['loggedin'] = $isloggedin;
-    $response['username'] = $username;
-    header('Content-Type: application/json');
-    echo(json_encode($response));    
->>>>>>> fa995461ccae52d1249e8f601c8f10c08d2723b6
 ?>
--- logout.php
+++ logout.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     // log user out by unsetting session variable called email, and destroying the session
     
@@ -14,21 +13,4 @@
     $response['status'] = 'success';
     header('Content-Type: application/json');
     echo(json_encode($response));    
-=======
-<?php
-    // log user out by unsetting session variable called email, and destroying the session
-    
-    session_start();
-    if (isset($_SESSION['username'])) {
-        // unsetting the username variable makes it so we assume that we are not logged in
-        unset($_SESSION['username']);
-    }
-    session_destroy();
-    
-    // send response back
-    $response = array();
-    $response['status'] = 'success';
-    header('Content-Type: application/json');
-    echo(json_encode($response));    
->>>>>>> fa995461ccae52d1249e8f601c8f10c08d2723b6
 ?>
