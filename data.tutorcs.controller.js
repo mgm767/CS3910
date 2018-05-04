b1f6902203c7c23e1f7563dbc0055edb6221c4c8
Michael Markopoulos <michael-markopoulos@uiowa.edu>
2018-05-03 22:23:40 -0500

    the scheduled sesisons for the tutor page

--- data.tutorcs.controller.js
+++ data.tutorcs.controller.js
@@ -96,6 +96,25 @@
                     }
                 });
         };
+
+        
+         $scope.sessions = [];
+        $scope.getSessions = function () {
+            $http.get('tutor_scheduled.php')
+                .then(function (response) {
+                    if (response.status == 200) {
+                        if (response.data.status == 'error') {
+                            alert('error: ' + response.data.message.sessions);
+                        } else {
+                            console.log(response.data.value.sessions);
+                            $scope.sessions = response.data.value.sessions;
+                        }
+                    } else {
+                        alert('unexpected error');
+                    }
+                });
+        };        
+        
         
         $scope.available_sessions_student = [];
         $scope.getAvailableSessions_student = function () {
